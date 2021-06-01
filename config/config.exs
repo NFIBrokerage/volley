38e75d54fceb57e7eb0a115b78a8ce362c3ac849
Michael Davis <mcarsondavis@gmail.com>
2021-06-01 11:43:06 -0500

    fix TLS config in config/config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,16 @@
 import Config
 
-host = System.get_env("EVENTSTORE_HOST") || "localhost"
+case System.get_env("EVENTSTORE_HOST") do
+  nil ->
+    config :volley, Volley.SpearClient,
+      connection_string: "esdb://admin:changeit@localhost:2113?tls=true",
+      mint_opts: [
+        transport_opts: [
+          cacertfile: Path.join([__DIR__ | ~w[.. certs ca ca.crt]])
+        ]
+      ]
 
-config :volley, Volley.SpearClient,
-  connection_string: "esdb://admin:changeit@#{host}:2113"
+  host ->
+    config :volley, Volley.SpearClient,
+      connection_string: "esdb://admin:changeit@#{host}:2113"
+end
