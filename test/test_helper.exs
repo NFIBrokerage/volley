4913338e0e5304e4743b8ebb16133e5b4b40af2d
Michael Davis <mcarsondavis@gmail.com>
2022-05-11 12:23:43 -0500

    ensure telemetry is started in test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,8 @@
 :ets.new(:stream_positions, [:set, :public, :named_table])
+Application.ensure_started(:telemetry)
 
-[Volley.SpearClient] |> Supervisor.start_link(strategy: :one_for_one)
+{:ok, _pid} =
+  [Volley.SpearClient] |> Supervisor.start_link(strategy: :one_for_one)
 
 ExUnit.configure(assert_receive_timeout: 1_000)
 ExUnit.start()
